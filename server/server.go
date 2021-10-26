29ce75b52d9378e20481fa9b98838db49e738241
Lasse Faurby <lasse_fk@hotmail.com>
2021-10-26 15:01:54 +0200

    Refactor
    Co-authored-by: Cronval <Cronval@users.noreply.github.com>
    Co-authored-by: NiksenB <NiksenB@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	Chat "Chitty_Chat/Chat"
 	"log"
 	"net"
-
-	"Chitty_Chat/Chat"
+	"time"
 
 	"google.golang.org/grpc"
 )
+
+type ChatServer struct {
+	Chat.UnimplementedChittyChatServiceServer
+}
+
+var messageList []*Chat.FromClient
 
 func main() {
 	listen, err := net.Listen("tcp", ":8007")
@@ -20,12 +26,71 @@ func main() {
 	grpcServer := grpc.NewServer()
 
 	// Creates instance of our ChittyChatServer struct and binds it with our empty gRPC server.
-	ccs := ChittyChatService.ChatServer{}
-	ChittyChatService.RegisterChittyChatServiceServer(grpcServer, &ccs)
+	ccs := ChatServer{}
+	Chat.RegisterChittyChatServiceServer(grpcServer, &ccs)
 
 	err = grpcServer.Serve(listen)
 	if err != nil {
 		log.Fatalf("Failed to start gRPC server :: %v", err)
 	}
 
+}
+
+//ChatService
+func (is *ChatServer) GetServerStream(ccsi Chat.ChittyChatService_GetServerStreamServer) error {
+
+	// receive request <<< client
+	go receiveFromStream(ccsi)
+
+	//stream >>> client
+	errch := make(chan error)
+	go sendToStream(ccsi, errch)
+
+	return <-errch
+}
+
+// receive from stream
+func receiveFromStream(ccsi_ Chat.ChittyChatService_GetServerStreamServer) {
+	for {
+		req, err := ccsi_.Recv()
+		if err != nil {
+			log.Printf("Error reciving request from client :: %v", err)
+			break
+
+		} else {
+			messageList = append(messageList, req)
+			log.Printf("%v", len(messageList))
+		}
+	}
+}
+
+//send to stream
+func sendToStream(ccsi_ Chat.ChittyChatService_GetServerStreamServer, errch_ chan error) {
+	for {
+
+		for {
+
+			time.Sleep(500 * time.Millisecond)
+
+			if len(messageList) == 0 {
+				break
+			}
+
+			message := messageList[0]
+
+			err := ccsi_.Send(&Chat.FromServer{Name: message.Name, Body: message.Body})
+
+			if err != nil {
+				errch_ <- err
+			}
+
+			if len(messageList) >= 2 {
+				messageList = messageList[1:]
+			} else {
+				messageList = []*Chat.FromClient{}
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
 }
