8a29919d4ef4985300ceb1b3714983c1976828d3
Lasse Faurby <lasse_fk@hotmail.com>
2021-11-02 12:11:02 +0100

    We done! Finish! Finito! :D

--- server/server.go
+++ server/server.go
@@ -3,12 +3,15 @@ package main
 import (
 	Chat "Chitty_Chat/Chat"
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
 
 	"google.golang.org/grpc"
 )
+
+var lamport int32
 
 type ChatServer struct {
 	Chat.UnimplementedChittyChatServiceServer
@@ -17,6 +20,8 @@ type ChatServer struct {
 var users = make(map[int32]Chat.ChittyChatService_JoinChatServer)
 
 func main() {
+	lamport = 0
+
 	listen, err := net.Listen("tcp", ":8007")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 8007: %v", err)
@@ -37,6 +42,11 @@ func main() {
 }
 
 func (c *ChatServer) JoinChat(user *Chat.User, ccsi Chat.ChittyChatService_JoinChatServer) error {
+	incomingLamport := user.Lamport
+
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
 	users[user.Id] = ccsi
 
 	defer func() {
@@ -46,7 +56,9 @@ func (c *ChatServer) JoinChat(user *Chat.User, ccsi Chat.ChittyChatService_JoinC
 		}
 	}()
 
-	Broadcast(&Chat.FromClient{Name: "ServerMessage", Body: user.Name + " has joined the chat"})
+	body := fmt.Sprintf(user.Name + " has joined the chat")
+
+	Broadcast(&Chat.FromClient{Name: "ServerMessage", Body: body, Lamport: lamport})
 
 	// block function
 	bl := make(chan bool)
@@ -56,14 +68,25 @@ func (c *ChatServer) JoinChat(user *Chat.User, ccsi Chat.ChittyChatService_JoinC
 }
 
 func (s *ChatServer) LeaveChat(ctx context.Context, user *Chat.User) (*Chat.Empty, error) {
+	incomingLamport := user.Lamport
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
 	delete(users, user.Id)
 	// updateTimestamp(int(user.Timestamp))
-	Broadcast(&Chat.FromClient{Name: "ServerMessage", Body: user.Name + " has left the chat"})
+	Broadcast(&Chat.FromClient{Name: "ServerMessage", Body: user.Name + " has left the chat at Lamport time ", Lamport: lamport})
 	return &Chat.Empty{}, nil
 }
 
 //ChatService
 func (is *ChatServer) Publish(ctx context.Context, msg *Chat.FromClient) (*Chat.Empty, error) {
+
+	incomingLamport := msg.Lamport
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
+	msg.Lamport = lamport
+
 	Broadcast(msg)
 	return &Chat.Empty{}, nil
 }
@@ -71,13 +94,22 @@ func (is *ChatServer) Publish(ctx context.Context, msg *Chat.FromClient) (*Chat.
 func Broadcast(msg *Chat.FromClient) {
 	name := msg.Name
 	body := msg.Body
+	time := msg.Lamport
 
-	log.Println(name + ": " + body)
+	log.Printf("%s : %s %d", name, body, time)
 
 	for key, value := range users {
-		err := value.Send(&Chat.FromServer{Name: name, Body: body})
+		err := value.Send(&Chat.FromServer{Name: name, Body: body, Lamport: time})
 		if err != nil {
 			log.Println("Failed to broadcast to "+string(key)+": ", err)
 		}
+	}
+}
+
+func max(x, y int32) int32 {
+	if x > y {
+		return x
+	} else {
+		return y
 	}
 }
